76429e1f30520a4b32d9dc75f7c2059e9f7bc462
Ruimin Gao <rgao76@gatech.edu>
2022-08-22 01:52:33 -0400

    Initial commit

---
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.helloworld
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val btnCoffee = findViewById<Button>(R.id.btnCoffee);
+        val ivFace = findViewById<ImageView>(R.id.ivFace);
+        val tvIntro = findViewById<TextView>(R.id.tvIntro);
+        btnCoffee.setOnClickListener {
+            Log.v("MainActivity", "btnCoffee is clicked");
+            Toast.makeText(this, "THX hope you have a good day XD!", Toast.LENGTH_SHORT);
+            ivFace.setImageResource(R.drawable.happy);
+            tvIntro.text = "Coffee ><!!";
+        }
+    }
+}
