e886676fabc57cc225678e28186c4790381f5c5b
Ruimin Gao <rgao76@gatech.edu>
2022-08-22 02:06:08 -0400

    Fix minor bugs

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -12,14 +12,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val btnCoffee = findViewById<Button>(R.id.btnCoffee);
-        val ivFace = findViewById<ImageView>(R.id.ivFace);
-        val tvIntro = findViewById<TextView>(R.id.tvIntro);
+        val btnCoffee = findViewById<Button>(R.id.btnCoffee)
+        val ivFace = findViewById<ImageView>(R.id.ivFace)
+        val tvIntro = findViewById<TextView>(R.id.tvIntro)
+        ivFace.setImageResource(R.drawable.sad)
         btnCoffee.setOnClickListener {
-            Log.v("MainActivity", "btnCoffee is clicked");
-            Toast.makeText(this, "THX hope you have a good day XD!", Toast.LENGTH_SHORT);
-            ivFace.setImageResource(R.drawable.happy);
-            tvIntro.text = "Coffee ><!!";
+            Log.v("MainActivity", "btnCoffee is clicked")
+            Toast.makeText(this, "THX hope you have a good day XD!", Toast.LENGTH_LONG).show()
+            ivFace.setImageResource(R.drawable.happy)
+            tvIntro.text = "Coffee ><!!"
         }
     }
 }
